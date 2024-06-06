6d8fe0c5abb8f56d3eb317e56b087575f523f968
超神的冰凉 <2801621420@qq.com>
2024-06-06 12:04:10 +0800

    zig 0.13.0-dev.351

--- build.zig
+++ build.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     // module exports
-    const module = b.addModule("JNI", .{ .root_source_file = .{ .path = "src/main/zig/lib.zig" } });
+    const module = b.addModule("JNI", .{ .root_source_file = .{ .src_path = .{
+        .owner = b,
+        .sub_path = "src/main/zig/lib.zig",
+    } } });
     module.addIncludePath(.{ .path = "src/include/jni" });
     module.link_libc = true;
     // build options
@@ -11,7 +14,10 @@ pub fn build(b: *std.Build) void {
     const lib = b.addStaticLibrary(.{
         .name = "zig-jni",
         .version = .{ .major = 0, .minor = 0, .patch = 1 },
-        .root_source_file = .{ .path = "src/main/zig/lib.zig" },
+        .root_source_file = .{ .src_path = .{
+            .owner = b,
+            .sub_path = "src/main/zig/lib.zig",
+        } },
         .target = target,
         .optimize = optimize,
     });
