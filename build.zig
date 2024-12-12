ccb475cb9f759997da0b3ec961915d220eeda671
FalsePattern <me@falsepattern.com>
2024-12-12 21:17:43 +0100

    feat: Simplify build.zig
    A static library is not really necessary here, zig code only uses the exported module

--- build.zig
+++ build.zig
@@ -1,22 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    // module exports
-    const module = b.addModule("JNI", .{ .root_source_file = .{ .src_path = .{
-        .owner = b,
-        .sub_path = "src/main/zig/lib.zig",
-    } } });
-    module.addIncludePath(.{ .src_path = .{
-        .owner = b,
-        .sub_path = "src/include/jni",
-    } });
-    module.link_libc = true;
     // build options
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
-    const lib = b.addStaticLibrary(.{
-        .name = "zig-jni",
-        .version = .{ .major = 0, .minor = 0, .patch = 1 },
+
+    // module exports
+    const module = b.addModule("JNI", .{
         .root_source_file = b.path("src/main/zig/lib.zig"),
         .target = target,
         .optimize = optimize,
@@ -27,11 +17,5 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
         .link_libc = true,
     });
-    lib.root_module.addImport("cjni", cjni.createModule());
-
-    lib.addIncludePath(.{ .src_path = .{
-        .owner = b,
-        .sub_path = "src/include/jni",
-    } });
-    b.installArtifact(lib);
+    module.addImport("cjni", cjni.createModule());
 }
